57aa50da23be08599fab6354be08d7c2916660e2
FlippinPizza <photography.flip@gmail.com>
2019-01-04 14:33:12 +0100

    First Commit

---
+++ src/main/java/com/github/testorganization/testrepository/TestRepositoryApplication.java
@@ -0,0 +1,23 @@
+package com.github.testorganization.testrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRepositoryApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String home() {
+		return"home";
+		
+	}
+
+}
+
---
+++ src/test/java/com/github/testorganization/testrepository/TestRepositoryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.testorganization.testrepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestRepositoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
